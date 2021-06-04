7c82291952206ca055e0d8857f764556873223ee
gonexwind <fikky.ardi15@gmail.com>
2021-06-04 21:53:31 +0700

    codelab 2

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -5,19 +5,23 @@ class DetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var informationTextStyle = TextStyle(fontFamily: 'Oxygen');
+
     return Scaffold(
-      body: SafeArea(
+      body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
+            Image.asset('images/farm-house.jpg'),
             Container(
               margin: EdgeInsets.only(top: 16),
               child: Text(
                 'Farm House Lembang',
-                textAlign: TextAlign.left,
+                textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 30,
                   fontWeight: FontWeight.bold,
+                  fontFamily: 'Staatliches',
                 ),
               ),
             ),
@@ -29,19 +33,19 @@ class DetailScreen extends StatelessWidget {
                   Column(
                     children: [
                       Icon(Icons.calendar_today),
-                      Text('Open Everyday'),
+                      Text('Open Everyday', style: informationTextStyle),
                     ],
                   ),
                   Column(
                     children: [
                       Icon(Icons.access_time),
-                      Text('09:00 - 20:00'),
+                      Text('09:00 - 20:00', style: informationTextStyle),
                     ],
                   ),
                   Column(
                     children: [
                       Icon(Icons.attach_money),
-                      Text('Rp. 25.000'),
+                      Text('Rp. 25.000', style: informationTextStyle),
                     ],
                   ),
                 ],
@@ -52,7 +56,39 @@ class DetailScreen extends StatelessWidget {
               child: Text(
                 'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instragramable.',
                 textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16),
+                style: informationTextStyle.copyWith(fontSize: 16),
+              ),
+            ),
+            Container(
+              height: 150,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(4),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.network(
+                          'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.network(
+                          'https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg'),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.network(
+                          'https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg'),
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
